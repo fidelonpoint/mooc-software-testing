1e2e06b3d3c38d7b32d1929668d5c001055731f3
fidelonpoint <fidelnonics@gmail.com>
2019-11-01 09:41:31 +0100

    solved gHappy exercise and wrote the automated test

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,28 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+    @Test
+    public void isHappy(){
+        GHappy happy = new GHappy();
+        boolean result = happy.gHappy("xxggxx");
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    public void isNotHappy(){
+        GHappy happy = new GHappy();
+        boolean result = happy.gHappy("xxgxx");
+        Assertions.assertEquals(false, result);
+    }
+
+    @Test
+    public void isNotHappy1(){
+        GHappy happy = new GHappy();
+        boolean result = happy.gHappy("xxggyygxx");
+        Assertions.assertEquals(false, result);
+    }
+
 }
